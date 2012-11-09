7e56fdf190bb8dfc718ceafb42de764a7dab1cf3
Dave Ross <dave@davidmichaelross.com>
2012-11-09 11:14:25 -0500

    varnish cache-busting headers

--- docroot/sites/default/modules/floodlight_tags/floodlight_numgen.php
+++ docroot/sites/default/modules/floodlight_tags/floodlight_numgen.php
@@ -3,7 +3,7 @@
 // A very thorough attempt to prevent caching
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); 
-header("Cache-Control: no-store, no-cache, must-revalidate"); 
+header("Cache-Control: s-maxage=0, max-age=0, no-store, no-cache, must-revalidate"); 
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
