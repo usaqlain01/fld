c3775833269d0717f22238d599233507aee4d04a
Dougie Dobrzynski <doug@promethost.com>
2013-09-09 10:26:12 -0500

    -n Updating Quicktabs module.

--- docroot/sites/all/modules/quicktabs/js/quicktabs_form.js
+++ docroot/sites/all/modules/quicktabs/js/quicktabs_form.js
@@ -1,13 +1,12 @@
-// $Id: quicktabs_form.js,v 1.5.4.2 2009/08/29 21:21:58 pasqualle Exp $
 (function ($) {
 
 Drupal.quicktabsShowHide = function() {
-  $(this).parents('tr').find('td.qt-tab-' + this.value + '-content').show().siblings('td.qt-tab-content').hide();
+  $(this).parents('tr').find('div.qt-tab-' + this.value + '-options-form').show().siblings('div.qt-tab-options-form').hide();
 };
 
 Drupal.behaviors.quicktabsform = {
   attach: function (context, settings) {
-    $('#quicktabs-form tr').not('.quicktabs-form-processed').addClass('quicktabs-form-processed').each(function(){
+    $('#quicktabs-form tr').once(function(){
       var currentRow = $(this);
       currentRow.find('div.form-item :input[name*="type"]').bind('click', Drupal.quicktabsShowHide);
       $(':input[name*="type"]:checked', this).trigger('click');
