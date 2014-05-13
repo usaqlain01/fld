5b5be00e49a99588aa5be5374b459be5c94a9410
Benjamin Doherty <bdoherty@caxy.com>
2014-05-13 11:28:13 -0500

    header markup.
    [touch: 94]

---
+++ themes/esquif/panels-layouts/canoe/esquif-canoe.tpl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file
+ * Template for a 2 column panel layout.
+ *
+ * This template provides a two column panel display layout, with
+ * each column roughly equal in width.
+ *
+ * Variables:
+ * - $id: An optional CSS id to use for the layout.
+ * - $content: An array of content, each item in the array is keyed to one
+ *   panel of the layout. This layout supports the following sections:
+ *   - $content['left']: Content in the left column.
+ *   - $content['right']: Content in the right column.
+ */
+?>
+<section class="l--module">
+  <?php print $content['left']; ?>
+</section>
+<section class="l--module">
+  <?php print $content['right']; ?>
+</section>
