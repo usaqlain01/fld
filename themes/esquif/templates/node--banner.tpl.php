b8113e5a89247781c0b6835ee2081dde8d66266b
Benjamin Doherty <bdoherty@caxy.com>
2014-05-22 15:54:26 -0500

    add contextual links to node banner view mode template.
    [touch: 92]

--- themes/esquif/templates/node--banner.tpl.php
+++ themes/esquif/templates/node--banner.tpl.php
@@ -1,3 +1,5 @@
 <a class="node-<?php print $node->nid; ?> banner" href="<?php print $node_url; ?>"<?php print $attributes; ?>>
+  <?php print render($title_prefix); ?>
+  <?php print render($title_suffix); ?>
   <?php print render($content); ?>
 </a>
