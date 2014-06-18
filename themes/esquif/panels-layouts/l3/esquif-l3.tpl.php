c592f21aad2d026c103ffb1beed9d6a4d5928f77
Benjamin Doherty <bjd@bangpound.org>
2014-06-18 15:39:44 -0500

    science landing page. l3 template.

--- themes/esquif/panels-layouts/l3/esquif-l3.tpl.php
+++ themes/esquif/panels-layouts/l3/esquif-l3.tpl.php
@@ -1,9 +1,43 @@
 <?php
-if ($main_classes) {
-  print '<div class="' . $main_classes . '">';
-  print $content['main'];
+print '<div class="l--body">';
+print '<div class="l--body__content">';
+
+if ($top_classes) {
+  print '<section class="' . $top_classes . '">';
+  print $content['top'];
+  print '</section>';
+}
+else {
+  print $content['top'];
+}
+
+if ($middle_classes) {
+  print '<div class="' . $middle_classes . '">';
+  print $content['middle'];
   print '</div>';
 }
 else {
-  print $content['main'];
+  print $content['middle'];
 }
+
+if ($bottom_classes) {
+  print '<section class="' . $bottom_classes . '">';
+  print $content['bottom'];
+  print '</section>';
+}
+else {
+  print $content['bottom'];
+}
+
+print '</div>';
+print '<div class="l--body__nav">';
+if ($nav_classes) {
+  print '<nav class="' . $nav_classes . '" role="navigation">';
+  print $content['nav'];
+  print '</nav>';
+}
+else {
+  print $content['nav'];
+}
+print '</div>';
+print '</div>';
