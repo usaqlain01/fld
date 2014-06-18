5f064ac5b05c570c6bd071a6aa28035b619f808c
Benjamin Doherty <bjd@bangpound.org>
2014-06-18 15:23:19 -0500

    l1 and l2 templates.

--- themes/esquif/panels-layouts/l2/esquif-l2.tpl.php
+++ themes/esquif/panels-layouts/l2/esquif-l2.tpl.php
@@ -1,9 +1,39 @@
 <?php
-if ($main_classes) {
-  print '<div class="' . $main_classes . '">';
-  print $content['main'];
+print '<div class="l--body">';
+print '<div class="l--body__content">';
+if ($callout_classes) {
+  print '<section class="' . $callout_classes . '">';
+  print $content['callout'];
+  print '</section>';
+}
+else {
+  print $content['callout'];
+}
+if ($top_classes) {
+  print '<div class="' . $top_classes . '">';
+  print $content['top'];
   print '</div>';
 }
 else {
-  print $content['main'];
+  print $content['top'];
 }
+if ($bottom_classes) {
+  print '<div class="' . $bottom_classes . '">';
+  print $content['bottom'];
+  print '</div>';
+}
+else {
+  print $content['bottom'];
+}
+print '</div>';
+print '<div class="l--body__nav">';
+if ($nav_classes) {
+  print '<nav class="' . $nav_classes . '" role="navigation">';
+  print $content['nav'];
+  print '</nav>';
+}
+else {
+  print $content['nav'];
+}
+print '</div>';
+print '</div>';
