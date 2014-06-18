a7c2a00675e15817cba6515175617e238b2a3ba7
Benjamin Doherty <bjd@bangpound.org>
2014-06-18 12:56:53 -0500

    add basic auth.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -80,3 +80,18 @@ if (file_exists('/var/www/site-php')) {
     }
   }
 }
+
+// Make sure Drush keeps working.
+// Modified from function drush_verify_cli()
+$cli = (php_sapi_name() == 'cli');
+if (!$cli) {
+  $username = 'fmnh';
+  $password = 'fmnh';
+  if (!(isset($_SERVER['PHP_AUTH_USER']) && ($_SERVER['PHP_AUTH_USER']==$username && $_SERVER['PHP_AUTH_PW']==$password))) {
+    header('WWW-Authenticate: Basic realm="This site is protected"');
+    header('HTTP/1.0 401 Unauthorized');
+    // Fallback message when the user presses cancel / escape
+    echo 'Access denied';
+    exit;
+  }
+}
