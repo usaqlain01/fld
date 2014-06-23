a5be593f19ad6e1aff0cc95846ff7a1189e8b422
Benjamin Doherty <bjd@bangpound.org>
2014-06-23 12:10:24 -0500

    support the museum landing page.

--- themes/esquif/panels-layouts/l5/esquif-l5.tpl.php
+++ themes/esquif/panels-layouts/l5/esquif-l5.tpl.php
@@ -1,9 +1,46 @@
 <?php
-if ($main_classes) {
-  print '<section class="' . $main_classes . '">';
-  print $content['main'];
+if ($callout_classes) {
+  print '<section class="' . $callout_classes . '">';
+  print $content['callout'];
   print '</section>';
 }
 else {
-  print $content['main'];
+  print $content['callout'];
 }
+?>
+<section class="summaries--2up">
+  <?php
+  if ($top_a_classes) {
+    print '<div class="' . $top_a_classes . '">';
+    print $content['top_a'];
+    print '</div>';
+  }
+  else {
+    print $content['top_a'];
+  }
+  if ($top_b_classes) {
+    print '<div class="' . $top_b_classes . '">';
+    print $content['top_b'];
+    print '</div>';
+  }
+  else {
+    print $content['top_b'];
+  }
+  if ($bottom_a_classes) {
+    print '<div class="' . $bottom_a_classes . '">';
+    print $content['bottom_a'];
+    print '</div>';
+  }
+  else {
+    print $content['bottom_a'];
+  }
+  if ($bottom_b_classes) {
+    print '<div class="' . $bottom_b_classes . '">';
+    print $content['bottom_b'];
+    print '</div>';
+  }
+  else {
+    print $content['bottom_b'];
+  }
+  ?>
+</section>
