119f5e8bf807c8fba761a8b3f3276a3a1d23aa24
Benjamin Doherty <bjd@bangpound.org>
2014-06-27 01:17:19 -0500

    calendar landing page.

--- themes/esquif/panels-layouts/l7/esquif-l7.tpl.php
+++ themes/esquif/panels-layouts/l7/esquif-l7.tpl.php
@@ -1,9 +1,26 @@
-<?php
-if ($main_classes) {
-  print '<div class="' . $main_classes . '">';
-  print $content['main'];
-  print '</div>';
-}
-else {
-  print $content['main'];
-}
+<div class="l--body">
+  <div class="l--body__content">
+    <?php
+    if ($main_classes) {
+      print '<div class="' . $main_classes . '">';
+      print $content['main'];
+      print '</div>';
+    }
+    else {
+      print $content['main'];
+    }
+    ?>
+  </div>
+  <div class="l--body__nav">
+    <?php
+    if ($nav_classes) {
+      print '<div class="' . $nav_classes . '">';
+      print $content['nav'];
+      print '</div>';
+    }
+    else {
+      print $content['nav'];
+    }
+    ?>
+  </div>
+</div>
