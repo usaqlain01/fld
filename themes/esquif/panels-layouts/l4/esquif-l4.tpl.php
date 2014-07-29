3e339119a3355a3e65865654f47e948f4dcf4b35
Benjamin Doherty <bjd@bangpound.org>
2014-07-29 02:29:01 -0500

    educators landing.

--- themes/esquif/panels-layouts/l4/esquif-l4.tpl.php
+++ themes/esquif/panels-layouts/l4/esquif-l4.tpl.php
@@ -1,67 +1,11 @@
 <section class="summaries--3up">
   <?php
-  if ($top_a_classes) {
-    print '<div class="' . $top_a_classes . '">';
-    print $content['top_a'];
+  if ($main_classes) {
+    print '<div class="' . $main_classes . '">';
+    print $content['main'];
     print '</div>';
   } else {
-    print $content['top_a'];
-  }
-  if ($top_b_classes) {
-    print '<div class="' . $top_b_classes . '">';
-    print $content['top_b'];
-    print '</div>';
-  } else {
-    print $content['top_b'];
-  }
-  if ($top_c_classes) {
-    print '<div class="' . $top_c_classes . '">';
-    print $content['top_c'];
-    print '</div>';
-  } else {
-    print $content['top_c'];
-  }
-  if ($middle_a_classes) {
-    print '<div class="' . $middle_a_classes . '">';
-    print $content['middle_a'];
-    print '</div>';
-  } else {
-    print $content['middle_a'];
-  }
-  if ($middle_b_classes) {
-    print '<div class="' . $middle_b_classes . '">';
-    print $content['middle_b'];
-    print '</div>';
-  } else {
-    print $content['middle_b'];
-  }
-  if ($middle_c_classes) {
-    print '<div class="' . $middle_c_classes . '">';
-    print $content['middle_c'];
-    print '</div>';
-  } else {
-    print $content['middle_c'];
-  }
-  if ($bottom_a_classes) {
-    print '<div class="' . $bottom_a_classes . '">';
-    print $content['bottom_a'];
-    print '</div>';
-  } else {
-    print $content['bottom_a'];
-  }
-  if ($bottom_b_classes) {
-    print '<div class="' . $bottom_b_classes . '">';
-    print $content['bottom_b'];
-    print '</div>';
-  } else {
-    print $content['bottom_b'];
-  }
-  if ($bottom_c_classes) {
-    print '<div class="' . $bottom_c_classes . '">';
-    print $content['bottom_c'];
-    print '</div>';
-  } else {
-    print $content['bottom_c'];
+    print $content['main'];
   }
   ?>
 </section>
