1371440030fddd10320a690c5881d7d7a6bcde3d
Benjamin Doherty <bjd@bangpound.org>
2014-07-29 03:35:08 -0500

    rearrange landing mini panels.

--- themes/esquif/panels-layouts/l5a/esquif-l5a.tpl.php
+++ themes/esquif/panels-layouts/l5a/esquif-l5a.tpl.php
@@ -1,22 +1,20 @@
-<?php
-print '<div class="l--body">';
-print '<div class="l--body__content">';
-
-if ($main_classes) {
-  print '<div class="' . $main_classes . '">';
-  print $content['main'];
-  print '</div>';
-} else {
-  print $content['main'];
-}
-print '</div>';
-print '<div class="l--body__nav">';
-if ($nav_classes) {
-  print '<nav class="' . $nav_classes . '" role="navigation">';
-  print $content['nav'];
-  print '</nav>';
-} else {
-  print $content['nav'];
-}
-print '</div>';
-print '</div>';
+<div class="l--body js--stickyParent">
+  <?php
+  if ($main_classes) {
+    print '<div class="' . $main_classes . '">';
+    print $content['main'];
+    print '</div>';
+  } else {
+    print $content['main'];
+  }
+  ?>
+  <?php
+  if ($nav_classes) {
+    print '<div class="' . $nav_classes . '">';
+    print $content['nav'];
+    print '</div>';
+  } else {
+    print $content['nav'];
+  }
+  ?>
+</div>
