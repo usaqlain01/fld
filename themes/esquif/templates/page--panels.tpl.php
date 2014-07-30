86fa79305eaff031d1523d8f8dc102aab2e50121
Benjamin Doherty <bjd@bangpound.org>
2014-07-29 20:08:31 -0500

    remove page title from panels page template.

--- themes/esquif/templates/page--panels.tpl.php
+++ themes/esquif/templates/page--panels.tpl.php
@@ -27,11 +27,6 @@
     <div class="l--content" role="main">
       <?php print render($page['highlighted']); ?>
       <?php print render($page['header']); ?>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
       <?php print $messages; ?>
       <?php print render($tabs); ?>
       <?php print render($page['help']); ?>
