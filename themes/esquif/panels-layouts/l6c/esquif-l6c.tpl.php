926c4af5a6ea79dc0b8e06495ad814ea98a6281b
Benjamin Doherty <bdoherty@caxy.com>
2014-07-31 01:38:44 -0500

    education landing pages.

--- themes/esquif/panels-layouts/l6c/esquif-l6c.tpl.php
+++ themes/esquif/panels-layouts/l6c/esquif-l6c.tpl.php
@@ -1,25 +1,20 @@
 <div class="l--body js--stickyParent">
-  <div class="l--body__content">
-    <?php
-    if ($main_classes) {
-      print '<div class="' . $main_classes . '">';
-      print $content['main'];
-      print '</div>';
-    } else {
-      print $content['main'];
-    }
-    ?>
-  </div>
+  <?php
+  if ($main_classes) {
+    print '<div class="' . $main_classes . '">';
+    print $content['main'];
+    print '</div>';
+  } else {
+    print $content['main'];
+  }    ?>
 
-  <div class="l--body__nav js--sticky" style="">
-    <?php
-    if ($nav_classes) {
-      print '<nav class="' . $nav_classes . '" role="navigation">';
-      print $content['nav'];
-      print '</nav>';
-    } else {
-      print $content['nav'];
-    }
-    ?>
-  </div>
+  <?php
+  if ($nav_classes) {
+    print '<div class="' . $nav_classes . '">';
+    print $content['nav'];
+    print '</div>';
+  } else {
+    print $content['nav'];
+  }
+  ?>
 </div>
