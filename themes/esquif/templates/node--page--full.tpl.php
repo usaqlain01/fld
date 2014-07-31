5322c4064da4bd092639c8a4733d0315064591a2
Benjamin Doherty <bdoherty@caxy.com>
2014-07-30 21:34:34 -0500

    main page template fixes

--- themes/esquif/templates/node--page--full.tpl.php
+++ themes/esquif/templates/node--page--full.tpl.php
@@ -9,18 +9,26 @@
 ?>
 <article class="article node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
+  <?php print render($title_prefix); ?>
   <h1<?php print $title_attributes; ?>><?php print $title; ?></h1>
+  <?php print render($title_suffix); ?>
+
   <?php print render($content['field_image']); ?>
+  <?php print render($content['field_summary']); ?>
+
   <section class="article__body"<?php print drupal_attributes(rdf_rdfa_attributes($node->rdf_mapping['body'])); ?>>
     <?php print render($content['body']); ?>
   </section>
+
   <?php
   // We hide the comments and links now so that we can render them later.
   hide($content['comments']);
   hide($content['links']);
   print render($content);
   ?>
+
   <?php print render($content['links']); ?>
+
   <?php print render($content['comments']); ?>
 
 </article>
