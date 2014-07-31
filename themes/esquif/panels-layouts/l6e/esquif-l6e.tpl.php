204472db93b2a1a2765778888c0b99335a51d742
Benjamin Doherty <bdoherty@caxy.com>
2014-07-31 02:35:33 -0500

    traveling exhibitions.

--- themes/esquif/panels-layouts/l6e/esquif-l6e.tpl.php
+++ themes/esquif/panels-layouts/l6e/esquif-l6e.tpl.php
@@ -1,35 +1,20 @@
 <div class="l--body js--stickyParent">
-  <div class="l--body__content">
-    <?php
-    if ($top_classes) {
-      print '<div class="' . $top_classes . '">';
-      print $content['top'];
-      print '</div>';
-    } else {
-      print $content['top'];
-    }
-    ?>
-    <hr class="rule--divider">
-    <?php
-    if ($bottom_classes) {
-      print '<div class="' . $bottom_classes . '">';
-      print $content['bottom'];
-      print '</div>';
-    } else {
-      print $content['bottom'];
-    }
-    ?>
-  </div>
+  <?php
+  if ($main_classes) {
+    print '<div class="' . $main_classes . '">';
+    print $content['main'];
+    print '</div>';
+  } else {
+    print $content['main'];
+  }    ?>
 
-  <div class="l--body__nav js--sticky">
-    <?php
-    if ($nav_classes) {
-      print '<nav class="' . $nav_classes . '" role="navigation">';
-      print $content['nav'];
-      print '</nav>';
-    } else {
-      print $content['nav'];
-    }
-    ?>
-  </div>
+  <?php
+  if ($nav_classes) {
+    print '<div class="' . $nav_classes . '">';
+    print $content['nav'];
+    print '</div>';
+  } else {
+    print $content['nav'];
+  }
+  ?>
 </div>
