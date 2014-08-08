15324a1ea7fe5072032310f5a4bd54f854242905
Benjamin Doherty <bjd@bangpound.org>
2014-08-08 03:38:32 -0500

    update dependencies.

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -8,7 +8,4 @@ $baseDir = dirname($vendorDir);
 return array(
     $vendorDir . '/kriswallsmith/assetic/src/functions.php',
     $vendorDir . '/raulfraile/ladybug-themes/Ladybug/Plugin/Extra/Resources/php/helpers.php',
-    $vendorDir . '/facebook/xhprof/xhprof_lib/utils/xhprof_lib.php',
-    $vendorDir . '/facebook/xhprof/xhprof_lib/utils/xhprof_runs.php',
-    $vendorDir . '/querypath/querypath/src/qp_functions.php',
 );
