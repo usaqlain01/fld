ffda8d036239c09b6e79170dc3952b0ab39e67fd
Benjamin Doherty <bdoherty@caxy.com>
2014-08-08 18:41:08 -0500

    home page.

--- themes/esquif/templates/fieldable-panels-pane--banner-description-and-list--naked.tpl.php
+++ themes/esquif/templates/fieldable-panels-pane--banner-description-and-list--naked.tpl.php
@@ -1,11 +1,19 @@
-<?php if ($content['field_image']): ?>
-<figure>
-  <?php print render($content['field_image']); ?>
-</figure>
+<?php if (isset($field_image)): ?>
+  <figure>
+    <?php print render($content['field_image']); ?>
+  </figure>
 <?php endif; ?>
 <hr class="rule--tight" />
 <?php print render($title_prefix); ?>
-<h4 class="scale--5"><a href="<?php print $url; ?>"><?php print $title; ?></a></h4>
+<h4 class="scale--5">
+  <?php if ($url): ?>
+  <a href="<?php print $url; ?>">
+    <?php endif; ?>
+    <?php print $title; ?>
+    <?php if ($url): ?>
+  </a>
+<?php endif; ?>
+</h4>
 <?php print render($title_suffix); ?>
 
 <div class="summary__description">
