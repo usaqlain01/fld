228d221e2f920b761b6a99028ca3245db75c5848
Benjamin Doherty <bjd@bangpound.org>
2014-08-11 00:38:27 -0500

    fix scroll to in calendar.

--- themes/esquif/templates/views-view-unformatted--calendar.tpl.php
+++ themes/esquif/templates/views-view-unformatted--calendar.tpl.php
@@ -7,7 +7,7 @@
  */
 ?>
 <?php if (!empty($title)): ?>
-  <h2 class="events__heading"><?php print $title; ?></h2>
+  <h2 class="events__heading" id="<?php print drupal_strtolower(trim($title)) ?>"><?php print $title; ?></h2>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
   <?php print $row; ?>
