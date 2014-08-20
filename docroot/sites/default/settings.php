dee5a27c42fa3fb3427846dc3a42e8a37d966737
Benjamin Doherty <bdoherty@caxy.com>
2014-08-20 15:48:34 -0500

    inhibit unauthorized access to test and dev environments.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -54,10 +54,21 @@ if (file_exists('/var/www/site-php')) {
     
     switch ($_ENV['AH_SITE_ENVIRONMENT']) {
       case 'dev':
-
-        break;
       case 'test':
-
+        // Make sure Drush keeps working.
+        // Modified from function drush_verify_cli()
+        $cli = (php_sapi_name() == 'cli');
+        if (!$cli) {
+          $username = 'fmnh';
+          $password = 'fmnh';
+          if (!(isset($_SERVER['PHP_AUTH_USER']) && ($_SERVER['PHP_AUTH_USER']==$username && $_SERVER['PHP_AUTH_PW']==$password))) {
+            header('WWW-Authenticate: Basic realm="This site is protected"');
+            header('HTTP/1.0 401 Unauthorized');
+            // Fallback message when the user presses cancel / escape
+            echo 'Access denied';
+            exit;
+          }
+        }
         break;
       case 'prod':
 
@@ -65,21 +76,3 @@ if (file_exists('/var/www/site-php')) {
     }
   }
 }
-
-// Make sure Drush keeps working.
-// Modified from function drush_verify_cli()
-/*
-$cli = (php_sapi_name() == 'cli');
-if (!$cli) {
-  $username = 'fmnh';
-  $password = 'fmnh';
-  if (!(isset($_SERVER['PHP_AUTH_USER']) && ($_SERVER['PHP_AUTH_USER']==$username && $_SERVER['PHP_AUTH_PW']==$password))) {
-    header('WWW-Authenticate: Basic realm="This site is protected"');
-    header('HTTP/1.0 401 Unauthorized');
-    // Fallback message when the user presses cancel / escape
-    echo 'Access denied';
-    exit;
-  }
-}
-
-*/
