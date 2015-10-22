17dbafb6c8c04d26e96c14b43f8e680b3349a0a4
Graham Troyer-Joy <gtroyerjoy@fieldmuseum.org>
2015-10-22 20:02:39 +0000

    updates 2015-10-22

--- docroot/profiles/fieldmuseum/modules/contrib/optimizely/js/optimizely-admin.js
+++ docroot/profiles/fieldmuseum/modules/contrib/optimizely/js/optimizely-admin.js
@@ -12,29 +12,29 @@
 
       // respond to checkbox being selected
       $('.form-checkbox').once().change(function() {
-        
+
         // Get the details of the clicked item
         var target_oid = $(this).attr('name').substring(8);
         var target_enable = $(this).is(':checked');
-        
+
         // Translate returned response to int value
         (target_enable == true) ? target_enable = 1 : target_enable = 0;
-  
+
         // Build string to pass values via 'POST'd
         var post = "target_oid=" + target_oid + "&target_enable=" + target_enable;
-        
+
         $.ajax({
           'url': Drupal.settings.basePath + 'admin/config/system/optimizely/ajax',
           'type': 'POST',
           'dataType': 'json',
           'data': post,
           'success': function(data) {
-            
+
             // Main selector for checkbox event
             var target_this = '#project-enable-' + data.oid;
-            
+
             if (data.status == "updated") {
-                      
+    
               // Toggle enable / disabled class
               if ($(target_this).attr('checked')) {
                 $(target_this).parents('tr').find('td').addClass('enabled').removeClass('disabled');
@@ -46,10 +46,10 @@
                 $(target_this).parents('tr').find('div.status-' + data.oid).attr("innerHTML","Project disabled successfully." + data.message);
                 $(target_this).parents('tr').find('div.status-' + data.oid).fadeOut(6000, function() { $(this).text('').css('display', ''); });
               }
-              
+
             }
             else {
-              
+
               // Reset checkbox
               if($(target_this).parents('tr').find('td').hasClass('disabled')) {
                 $('input[name=project-' + data.oid + ']').removeAttr('checked');
@@ -61,30 +61,30 @@
               $(target_this).parents('tr').find('div.status-' + data.oid).text(data.message);
               $(target_this).parents('tr').find('div.status-' + data.oid).fadeOut(6000, function() { $(this).text('').css('display', ''); });
             }
-            
+
           },
           'beforeSend': function() {
-            
+
             $(document).ready(function (data) {
-              
+
               var target_this = '#project-enable-' + data.oid;
 
               $(target_this).parents('tr').find('div.status-' + data.oid).text('Loading....');
               $(target_this).parents('tr').find('div.status-' + data.oid).fadeOut(6000, function() { $(this).text('').css('display', ''); });
-              
+
             });
-            
+
           },
           'error': function (data, status, error) {
-                     
+   
             $(document).ready(function () {
 
               var target_this = '#project-enable-' + data.oid;
               $(target_this).parents('tr').find('div.status-' + data.oid).text('ERROR OCCURRED!');
               $(target_this).parents('tr').find('div.status-' + data.oid).fadeOut(6000, function() { $(this).text('').css('display', ''); });
-              
+
             });
-            
+
           }
 
         });
